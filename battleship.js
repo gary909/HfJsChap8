00691ffa42a5365e7eff67fbf2fb98cbb1ff09fc
gary <gary.white@lodgit.com>
2021-03-30 16:19:56 +0200

    code

---
+++ battleship.js
@@ -0,0 +1,11 @@
+var view = {
+    displayMessage: function(msg) {
+        var messageArea = document.getElementById("messageArea");
+        messageArea.innerHTML = msg;
+    },
+    displayHit: function(location) {    
+    },
+    displayMiss: function(location) {
+    }
+};
+
