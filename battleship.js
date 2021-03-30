d787819827796a97bc26c4cf22a5b821584808a7
gary <garywhite61@hotmail.com>
2021-03-30 17:27:31 +0200

    code stuff

--- battleship.js
+++ battleship.js
@@ -27,7 +27,14 @@ var model = {
 
         for (var i = 0; i < this.numShips; i++) {
             var ship = this.ships[i];
+            locations = ship.locations;
+            var index = locations.indexOf(guess);
+            if (index >= 0) {
+                ship.hits[index] = "hit";
+                return true;
+            }
         }
+        return false;
     }       
 };
 
