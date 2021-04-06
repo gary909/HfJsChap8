a11518aee6eeec975b64a15cd978292c8ccc65a2
gary <garywhite61@hotmail.com>
2021-04-06 16:53:18 +0200

    game over code

--- battleship.js
+++ battleship.js
@@ -57,7 +57,14 @@ var controller = {
     guesses: 0,
 
     processGuess: function(guess) {
-        // more code will go here
+        var location = parseGuess(guess);
+        if (location) {
+            this.guesses++;
+            var hit = model.fire(location);
+            if (hit && model.shipsSunk === model.numShips) {
+                view.displayMessage("You sank all of my battleships, in " + this.guesses + " guesses");
+            }
+        }
     }
 };
 
@@ -91,12 +98,32 @@ function parseGuess(guess) {
 
 // view.displayMessage("Is this working??")
 
-model.fire("53");
+// model.fire("53");
 
-model.fire("06");
-model.fire("16");
-model.fire("26");
+// model.fire("06");
+// model.fire("16");
+// model.fire("26");
 
-model.fire("12");
-model.fire("11");
-model.fire("10");
+// model.fire("12");
+// model.fire("11");
+// model.fire("10");
+
+// console.log(parseGuess("A0"));
+// console.log(parseGuess("B6"));
+// console.log(parseGuess("G3"));
+// console.log(parseGuess("H0")); //null
+// console.log(parseGuess("A7")); //null
+
+controller.processGuess("A0");
+
+controller.processGuess("A6");
+controller.processGuess("B6");
+controller.processGuess("C6");
+
+controller.processGuess("C4");
+controller.processGuess("D4");
+controller.processGuess("E4");
+
+controller.processGuess("B0");
+controller.processGuess("B1");
+controller.processGuess("B2");
