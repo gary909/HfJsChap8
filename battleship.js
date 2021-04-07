5b1bac3dc3efbcfdee6a0711b6a86d7b28f8f25b
gary <garywhite61@hotmail.com>
2021-04-07 16:57:16 +0200

    working code

--- battleship.js
+++ battleship.js
@@ -92,14 +92,24 @@ function parseGuess(guess) {
 function init() {
     var fireButton = document.getElementById("fireButton");
     fireButton.onclick = handleFireButton;
+    var guessInput = document.getElementById("guessInput");
+    guessInput.onkeypress = handleKeyPress;
 }
 
 function handleFireButton() {
     var guessInput = document.getElementById("guessInput");
-    var guess = guessInput.nodeValue;
+    var guess = guessInput.value;
     controller.processGuess(guess);
 
     guessInput.value = ""; // resets the text form 
+}
+
+function handleKeyPress(e) {
+    var fireButton = document.getElementById("fireButton");
+    if (e.keyCode === 13) {
+        fireButton.click();
+        return false;
+    }
 }
 
 window.onload = init;
