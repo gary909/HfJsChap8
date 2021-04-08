6f1a094c3a498739c419e49115818e165ec51e15
gary <garywhite61@hotmail.com>
2021-04-08 17:45:18 +0200

    bug fix - now finished

--- battleship.js
+++ battleship.js
@@ -70,12 +70,13 @@ var model = {
         var direction = Math.floor(Math.random() * 2);
         var row;
         var col;
+
         if (direction === 1) {
             row = Math.floor(Math.random() * this.boardSize);
             col = Math.floor(Math.random() * (this.boardSize - this.shipLength + 1))
         } else {
-            row = Math.floor(Math.random() * this.boardSize - this.shipLength + 1);
-            col = Math.floor(Math.random() * (this.boardSize))
+            row = Math.floor(Math.random() * (this.boardSize - this.shipLength + 1));
+            col = Math.floor(Math.random() * this.boardSize);
         }
 
         var newShipLocations = [];
@@ -91,7 +92,7 @@ var model = {
 
     collision: function(locations) {
         for (var i = 0; i < this.numShips; i++) {
-            var ship = model.ships[i];
+            var ship = this.ships[i];
             for (var j = 0; j < locations.length; j++) {
                 if (ship.locations.indexOf(locations[j]) >= 0) {
                     return true;
